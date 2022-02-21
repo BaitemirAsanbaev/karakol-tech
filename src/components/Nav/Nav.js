ec389138fe586cb1ca367acd83d8075a82cea743
Aimarall <aimaral21072004@gmail.com>
2022-02-21 15:52:21 +0600

    Create nav structure

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import Logo from "../Logo/Logo";
+
+const Nav = () => {
+  return ( <nav>
+    <ul>
+      <li><a>Курсы</a></li>
+      <li><a>Отзывы</a></li>
+      <li><a><Logo/></a></li>
+      <li><a>О нас</a></li>
+      <li><a>Контакты</a></li>
+    </ul>
+  </nav> );
+}
+ 
+export default Nav;
