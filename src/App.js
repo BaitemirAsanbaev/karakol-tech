bab846ac0976e87e52e5c14269af23fc8afd8932
Tatyna Kubatova <tatynakubatova74@gmail.com>
2022-02-28 21:08:27 +0600

    Header structure & styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from './components/Nav/Nav';
 import './App.scss';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Home/>
     </div>
   );
 }
