719847f3ea52a4380ec3c9dcf1ac1a1f38895a7a
Baitemir Asanbaev <baitemir1706@gmail.com>
2022-03-07 21:03:15 +0600

    created footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Footer/>
     </div>
-  );
+);
 }
 
 export default App;
