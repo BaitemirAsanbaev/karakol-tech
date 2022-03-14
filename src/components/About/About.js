35dc1ab0a6e8c9ba1d41f695a6fff4319250bff4
Tatyna Kubatova <tatynakubatova74@gmail.com>
2022-03-14 14:13:21 +0600

    Change scss structure

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (<div className={classes.About}>
     <h1>О нас</h1>
     <p className={classes.AboutText}>Главная цель этого формата — сделать из тебя профессионального младшего разработчика. Все составляющие готовят тебя к реальным задачам, которые ты встретишь при работе на компанию или фрилансе. </p>
-    <div className={classes.CardContainer}>
+    <div className={classes.CardContainer}>ч
       <div className={classes.Card}>
         <img src={aboutIcon} width='60'/>
         <h2>Упор на практику</h2>
