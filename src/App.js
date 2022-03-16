d698f16b6f337476379e4c8b0bb2957d51d26175
Baitemir Asanbaev <baitemir1706@gmail.com>
2022-03-16 14:20:15 +0600

    added feedback

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
+import Feedback from './components/Feedback/Feedback';
 import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Feedback/>
       <Footer/>
     </div>
 );
---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,35 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import { Navigation, A11y } from 'swiper';
+import { feedbacks } from './feedbacks';
+import classes from "./Feedback.module.scss";
+import "../../App.scss"
+
+
+const result = feedbacks.map((item)=>{
+  return(
+    <SwiperSlide key={item.author}>
+      
+    <div className={classes.container}>
+      <p>{item.text}</p>
+      <span>-{item.author}</span>
+    </div>
+    </SwiperSlide>
+  )
+})
+
+const Feedback = () => {
+  return (<div className={classes.Feedback}>
+    <Swiper
+      modules={[Navigation, A11y]}
+      spaceBetween={10}
+      slidesPerView={1}
+      navigation
+      autoplay={true}>
+        {result}
+    </Swiper>
+  </div>);
+}
+
+export default Feedback;
